e2e58b4dfc1444baab7fecc356b9c5bd0ac28611
Laura Ferreira <laura.losa.ferreira@gmail.com>
2026-04-26 17:40:58 +0100

    feat: Scraping de 110 itens

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -5,12 +5,12 @@ def main():
     # Example collection:  https://repositorium.uminho.pt/collections/690f7814-a67b-4f27-8fff-6b33581d1a91/search
     # https://repositorium.uminho.pt/handle/1822/21293
     repo_url = f"https://repositorium.uminho.pt/handle/"
-    collection = "1822/21293"
+    collection = "1822/14400"
     base_url = f"{repo_url}/{collection}"
 
     # Create an instance of the Scraper class
     # The scraper will automatically detect Chrome in default locations
-    scraper_instance = scraper.UMinhoDSpace8Scraper(base_url, max_items=15)
+    scraper_instance = scraper.UMinhoDSpace8Scraper(base_url, max_items=110)
     final_results = scraper_instance.scrape()
 
     print(f"Scraping completed. Total papers scraped: {len(final_results)}")
